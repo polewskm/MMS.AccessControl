45235303c66832b9d8e063e12bb1c726b0e511ca
Marcin Polewski <marcin.polewski@gmail.com>
2019-03-09 19:54:59 -0600

    Initial commit

---
+++ MMS.AccessControl.DbDeploy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MMS.AccessControl.DbDeploy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MMS.AccessControl/AuthenticationRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MMS.AccessControl
+{
+    public class AuthenticationRequest
+    {
+        public string ReaderId { get; set; }
+        public int? MemberId { get; set; }
+        public string KeyCode { get; set; }
+    }
+
+    public static class AuthenticationResultCodes
+    {
+        public const int Success = 0;
+        public const int Error = 1;
+        public const int InvalidReader = 2;
+        public const int InvalidKeyCode = 3;
+    }
+
+    public class AuthenticationResult
+    {
+        public int ResultCode { get; set; }
+        public Member Member { get; set; }
+        public string[] Roles { get; set; }
+    }
+
+    public class Member
+    {
+        public int MemberId { get; set; }
+        public string FullName { get; set; }
+        public DateTimeOffset JoinedWhen { get; set; }
+        public DateTimeOffset ExpiresWhen { get; set; }
+    }
+}
